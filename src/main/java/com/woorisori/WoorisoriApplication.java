88f7cc4fdd844009e39f982821736813c301fb59
Lee Yunbi <dbsql10145@gmail.com>
2025-04-27 16:30:20 +0900

    Add initial build.gradle

---
+++ src/main/java/com/woorisori/WoorisoriApplication.java
@@ -0,0 +1,13 @@
+package com.woorisori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoorisoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoorisoriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woorisori/WoorisoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woorisori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoorisoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
