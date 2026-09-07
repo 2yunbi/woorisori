5f4f1721ca1767ec16f975b47f8a5ee396954881
Lee Yunbi <dbsql10145@gmail.com>
2026-09-07 22:56:11 +0900

    feat : 고충처리(complaint) 테이블 DDL 추가
    - Complaint 엔티티에 대응하는 complaint 테이블 생성 쿼리 작성

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -4,17 +4,34 @@ CREATE DATABASE woorisori
 
 use woorisori;
 
-drop table if exists member cascade;member
+-- 사용자 테이블
+drop table if exists member cascade;
 create table member
 (
     id bigint primary key,
-    emp_no varchar(10) Not null unique,
+    emp_no varchar(10) not null unique,
     password varchar(255) not null,
     user_name varchar(50) not null,
     email varchar(200) not null unique,
     role varchar(20) default 'USER',
     is_use varchar(1) default 'Y',
     join_date datetime default CURRENT_TIMESTAMP,
-    modify_date datetime default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    modify_date datetime default CURRENT_TIMESTAMP on update current_timestamp,
     delete_date datetime default CURRENT_TIMESTAMP
 )
+-- 고충처리 게시판 테이블
+drop table if exists complaint cascade;
+create table complaint (
+   id bigint auto_increment primary key,
+   writer_id bigint not null,
+   subject varchar(100) not null,
+   content text not null,
+   status varchar(100) not null,
+   create_date datetime not null default CURRENT_TIMESTAMP,
+   modify_date datetime not null default CURRENT_TIMESTAMP on update current_timestamp,
+   delete_date datetime null,
+
+   constraint fk_writer_id foreign key (writer_id)
+       references member(id)
+       on delete restrict
+);
