7784437468b51a5ccef39fdc5628a91e21e10c3e
Lee Yunbi <dbsql10145@gmail.com>
2026-09-11 13:28:09 +0900

    feat: ComplaintDto 수정
    - Getter, Setter 추가

--- src/main/java/com/woorisori/complaint/dto/ComplaintDto.java
+++ src/main/java/com/woorisori/complaint/dto/ComplaintDto.java
@@ -1,9 +1,12 @@
 package com.woorisori.complaint.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
 public class ComplaintDto {
 
+    @Getter @Setter
     public static class WriteForm {
 
         @NotBlank(message = "제목을 작성해주세요.")
